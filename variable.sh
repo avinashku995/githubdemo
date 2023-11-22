82068d687ae0e923139863fd7edfbef3e791f169
avinashku995 <avinashku995@gmail.com>
2023-11-22 11:11:38 +0530

    try commit

---
+++ variable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Purpose :- Variable"
+echo "Author Name :- Bash Shell Scripting"
+echo -e "\033[0;31m Learn Variable"
+
+echo "Type Of Variable" 
+echo "User Define Variable" 
+echo "System Define Variable"
+echo -e "\033[0;33m Type Of Variable"
+
+echo "User Define Variable"
+var="rahul"
+age="23"
+echo "This person $var and person $age"
+echo -e "\033[0;32m Learn :- User Define Variable"
+
+var="Database Management System"
+var2="mysql"
+var3="sql"
+echo "Database Engineer Learn $var and $var2 and $var3"
+
